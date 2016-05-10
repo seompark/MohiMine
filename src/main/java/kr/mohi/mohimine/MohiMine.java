c29a47cd9660d9830ab0dbb62c971336c283e3d6
MohiPE <0428asd@naver.com>
2016-05-11 04:05:49 +0900

    save

---
+++ src/main/java/kr/mohi/mohimine/MohiMine.java
@@ -0,0 +1,12 @@
+package kr.mohi.mohimine;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class MohiMine extends PluginBase {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+
+}
---
+++ src/main/java/kr/mohi/mohimine/task/initMine.java
@@ -0,0 +1,16 @@
+package kr.mohi.mohimine.task;
+
+import cn.nukkit.scheduler.AsyncTask;
+
+public class initMine extends AsyncTask {
+	
+	public initMine() {
+		super();
+	}
+	@Override
+	public void onRun() {
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+
+}
